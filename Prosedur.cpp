77f947e32f2c71934817f9dfd7263d7aca3d84b1
Daffansyah276 <daffansyaharya@gmail.com>
2025-03-06 21:01:23 +0700

    first commit

---
+++ Prosedur.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+    //numeric panjang, lebar, luas
+    double panjang;
+    double lebar;
+    double luas;
+    void inputdata() {
+    //display 'masukkan panjangnya = '
+    cout << "masukan panjangnya = ";
+    //accpet panjang
+    cin >> panjang;
+    //display 'masukkan lebarnya = '
+    cout << "masukkan lebarnya = ";
+    //accept lebar
+    cin >> lebar;
+    }
+   double hitungluas() {
+    //compute luas = panjang * lebar
+    luas = panjang * lebar;
+    return luas;
+}
+
+void tampildata() {
+  //display 'luas persegi panjang = ' + luas
+  cout << "luasnya adalah " << hitungluas(); 
+}
+    
+int main()
+{//begin
+    inputdata();
+    tampildata();
+
+}//end
